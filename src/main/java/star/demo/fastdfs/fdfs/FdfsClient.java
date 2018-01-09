796aba6c30ae8d7112df9bee2b00d266ca3d66df
jinweile <jinweile@163.com>
2018-01-09 11:10:15 +0800

    增加fastdfs的client

---
+++ src/main/java/star/demo/fastdfs/fdfs/FdfsClient.java
@@ -0,0 +1,63 @@
+package star.demo.fastdfs.fdfs;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.csource.common.NameValuePair;
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient;
+import org.csource.fastdfs.StorageServer;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+public class FdfsClient {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final String CLIENT_CONFIG_FILE = "fdfs/fdfs_client.conf";
+
+    private static TrackerClient trackerClient;
+    private static TrackerServer trackerServer;
+    private static StorageServer storageServer;
+    private static StorageClient storageClient;
+
+    static {
+        try {
+            String classPath = new File(FdfsClient.class.getResource("/").getFile()).getCanonicalPath();
+            String fdfsClientConfigFilePath = classPath + File.separator + CLIENT_CONFIG_FILE;
+            ClientGlobal.init(fdfsClientConfigFilePath);
+            trackerClient = new TrackerClient();
+            trackerServer = trackerClient.getConnection();
+            storageClient = new StorageClient(trackerServer, storageServer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * <strong>方法概要： 文件上传</strong> <br>
+     * <strong>创建时间： 2016-9-26 上午10:26:11</strong> <br>
+     *
+     * @param fileContent
+     *            file
+     * @return Url
+     */
+    public static String upload(byte[] fileContent, String extName, NameValuePair[] valuePairs) {
+        String[] uploadResults = null;
+        try {
+            uploadResults = storageClient.upload_file(fileContent,extName, valuePairs);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String groupName = uploadResults[0];
+        String remoteFileName = uploadResults[1];
+
+        String Url = groupName + "/" + remoteFileName;
+        return Url;
+    }
+
+}
--- src/main/java/star/demo/fastdfs/web/ImgController.java
+++ src/main/java/star/demo/fastdfs/web/ImgController.java
@@ -1,12 +1,15 @@
 package star.demo.fastdfs.web;
 
+import org.csource.common.NameValuePair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import star.demo.fastdfs.db.ImgDao;
+import star.demo.fastdfs.fdfs.FdfsClient;
 import star.demo.fastdfs.model.Img;
 
 import java.util.HashMap;
@@ -37,12 +40,24 @@ public class ImgController {
     }
 
     @RequestMapping("/upload")
-    @ResponseBody
     public String upload(
-            String name
-    ){
+            MultipartFile file
+    ) throws Exception {
+        String ext = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);
+        NameValuePair[] meta_list = new NameValuePair[4];
+        meta_list[0] = new NameValuePair("fileName", file.getOriginalFilename());
+        meta_list[1] = new NameValuePair("fileLength", String.valueOf(file.getSize()));
+        meta_list[2] = new NameValuePair("fileExt", ext);
+        meta_list[3] = new NameValuePair("fileAuthor", "WangLiang");
+        String url = FdfsClient.upload(file.getBytes(), ext, meta_list);
 
-        return "true";
+        Img img = new Img();
+        img.setImgName(file.getOriginalFilename());
+        img.setImgType(file.getContentType());
+        img.setImgUrl(url);
+        imgDao.insert(img);
+
+        return "redirect:/index";
     }
 
 }
